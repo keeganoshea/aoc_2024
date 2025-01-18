56751af0c204756f7630e044b8086cb5026b830d
keeganoshea <koshea@doximity.com>
2025-01-18 00:10:44 -0700

    Part 2

--- 11/problem.rb
+++ 11/problem.rb
@@ -28,10 +28,37 @@ def part1(data)
   post_blink_stones.length
 end
 
-def part2(*data)= data
+def part2(data)
+  blinks = 75
+  initial_stones = data.split(" ").map(&:to_i)
+  post_blink_stones_hash = initial_stones.dup.each_with_object(Hash.new(0)) do |stone, hash|
+    hash[stone] += 1
+  end
+
+  blinks.times do |i|
+    stones = post_blink_stones_hash
+    post_blink_stones_hash = Hash.new(0)
+    stones.each do |stone, count|
+      if stone.zero?
+        post_blink_stones_hash[1] += count
+      elsif stone.to_s.length.even?
+        mid = stone.to_s.size / 2
+        post_blink_stones_hash[stone.to_s[0...mid].to_i] += count
+        post_blink_stones_hash[stone.to_s[mid..-1].to_i] += count
+      else
+        post_blink_stones_hash[2024 * stone] += count
+      end
+    end
+  end
+  post_blink_stones_hash.values.sum
+end
 
 # output(1, :test_input)
-output(1, :input)
+# output(1, :input)
 
-# output(2, :example)
+start_time = Time.now
+output(2, :input)
+end_time = Time.now
+puts "Took: #{end_time - start_time} seconds"
+
 # output(2, :input)
